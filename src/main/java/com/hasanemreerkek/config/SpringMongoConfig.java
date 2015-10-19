3c42ad5904df5534af2c7935decd4de3a259d168
heerkek <h.emre.erkek@gmail.com>
2015-10-19 17:26:27 +0300

    fixed mongodb conf

--- src/main/java/com/hasanemreerkek/config/SpringMongoConfig.java
+++ src/main/java/com/hasanemreerkek/config/SpringMongoConfig.java
@@ -3,24 +3,20 @@ package com.hasanemreerkek.config;
 /**
  * Created by Emre on 13.10.2015.
  */
+import com.mongodb.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
-import com.mongodb.MongoClient;
+import java.net.UnknownHostException;
 
 @Configuration
 public class SpringMongoConfig {
+    
 
-    @Bean
-    public DB getDb() throws UnknownHostException, MongoException {
-        MongoURI mongoURI = new MongoURI(System.getenv("MONGOHQ_URL"));
-        DB db = mongoURI.connectDB();
-        db.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
 
-        return db;
-    }
 
 }
